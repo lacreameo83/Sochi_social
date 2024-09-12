146e1d8c4024a8a506063467d87450a67d90bf7b
precious osonwa <osonwap083@gmail.com>
2024-09-12 09:22:15 +0100

    making changes

--- preshMedia/src/pages/Mainpage.jsx
+++ preshMedia/src/pages/Mainpage.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-import MenuLeft from "../components/outlet/menuLeft";
 import Feed from "../components/outlet/Feed";
-import Rightbar from "../components/outlet/Rightbar";
 
 function Mainpage() {
   return (
